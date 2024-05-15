58f41589e7424bf6a3025027a7fc9595936c1411
AnntonySmith <ankab.ti@gmail.com>
2024-05-15 11:13:34 +0300

    Add files via upload

---
+++ lr.h
@@ -0,0 +1,18 @@
+#ifndef LR_H
+#define LR_H
+#include <stdio.h>
+
+void lab1();
+void lab2();
+void lab2dop();
+void lab3();
+void lab3dop();
+void lab4();
+void lab4dop();
+void lab5();
+void lab5dop();
+void lab6();
+void lab6dop();
+void lab7();
+
+#endif /*LR_H*/
